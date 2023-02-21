bafe0613852726b421b95fd8f2e1b1082db5358d
wcollins <william.levi.collins@gmail.com>
2023-02-21 10:41:12 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,3 +12,23 @@ variable "region" {
   type        = string
   default     = "us-east-2"
 }
+
+# variable "vpc_name" {
+#   description  = "Name of AWS VPC"
+#   type         = string
+# }
+
+# variable "vpc_cidr" {
+#   description  = "CIDR of AWS VPC"
+#   type         = string
+# }
+
+# variable "subnet_name" {
+#   description  = "Name of AWS subnet"
+#   type         = string
+# }
+
+# variable "subnet_cidr" {
+#   description  = "CIDR of AWS subnet"
+#   type         = string
+# }
