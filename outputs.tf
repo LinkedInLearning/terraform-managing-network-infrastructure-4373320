629199f926e8fe69a176125369ccabf8c8312b1f
wcollins <william.levi.collins@gmail.com>
2023-02-25 12:56:39 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "subnet_id" {
   description  = "ID of AWS subnets"
   value        = aws_subnet.dev.*.id
 }
+
+output "instance_id" {
+  description  = "ID of AWS instances"
+  value        = aws_instance.dev.*.id
+}
+
+output "public_ip" {
+  description  = "Public IP of AWS instances"
+  value        = aws_instance.dev.*.public_ip
+}
