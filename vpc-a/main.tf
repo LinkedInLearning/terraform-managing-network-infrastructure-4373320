3266cc5653f1944701edf9dfa4ed5252ad898c1b
wcollins <william.levi.collins@gmail.com>
2023-03-17 15:40:16 -0400

    Add tgw config to main.tf

--- vpc-a/main.tf
+++ vpc-a/main.tf
@@ -18,3 +18,23 @@ resource "aws_subnet" "dev" {
   }
 
 }
+
+resource "aws_ec2_transit_gateway" "tgw" {
+  description = "Managed by Terraform" 
+
+  tags = {
+    Name = format("tgw-%s", var.region)
+  }
+
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "dev" {
+  vpc_id              = aws_vpc.dev.id
+  subnet_ids          = [aws_subnet.dev[0].id]
+  transit_gateway_id  = aws_ec2_transit_gateway.tgw.id
+
+  tags = {
+    Name = format("attachment-%s", var.vpc_name)
+  }
+
+}
--- vpc-b/main.tf
+++ vpc-b/main.tf
@@ -1,3 +1,12 @@
+data "terraform_remote_state" "vpc_a" {
+  backend = "local"
+
+  config  = {
+    path  = "../vpc-a/terraform.tfstate"
+  }
+
+}
+
 resource "aws_vpc" "test" {
   cidr_block  = var.vpc_cidr
 
@@ -31,8 +40,13 @@ resource "aws_route_table" "test" {
   vpc_id = aws_vpc.test.id
 
   route {
-    cidr_block  = "0.0.0.0/0"
-    gateway_id  = aws_internet_gateway.test.id
+    cidr_block         = "10.0.0.0/8"
+    transit_gateway_id =  data.terraform_remote_state.vpc_a.outputs.tgw_id
+  }
+
+  route {
+    cidr_block         = "0.0.0.0/0"
+    gateway_id         = aws_internet_gateway.test.id
   }
 
   tags    = {
@@ -44,4 +58,15 @@ resource "aws_route_table" "test" {
 resource "aws_route_table_association" "test" {
   subnet_id       = aws_subnet.test.id
   route_table_id  = aws_route_table.test.id
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "test" {
+  vpc_id              = aws_vpc.test.id
+  subnet_ids          = [aws_subnet.test.id]
+  transit_gateway_id  = data.terraform_remote_state.vpc_a.outputs.tgw_id 
+
+  tags = {
+    Name = format("attachment-%s", local.vpc_name)
+  }
+
 }
