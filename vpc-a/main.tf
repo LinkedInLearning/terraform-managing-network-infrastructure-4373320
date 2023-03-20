6ddb5087e474a2f2aa0cf55c3d3dec026641dbe7
wcollins <william.levi.collins@gmail.com>
2023-03-20 17:33:55 -0400

    Update main.tf

--- vpc-a/main.tf
+++ vpc-a/main.tf
@@ -20,7 +20,13 @@ resource "aws_subnet" "dev" {
 }
 
 resource "aws_ec2_transit_gateway" "tgw" {
-  description = "Managed by Terraform" 
+  description                     = "Managed by Terraform" 
+
+  # Disable association with default route table
+  default_route_table_association = "disable"
+
+  # Disable propagation to default route table
+  default_route_table_propagation = "disable"
 
   tags = {
     Name = format("tgw-%s", var.region)
@@ -32,6 +38,12 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "dev" {
   vpc_id              = aws_vpc.dev.id
   subnet_ids          = [aws_subnet.dev[0].id]
   transit_gateway_id  = aws_ec2_transit_gateway.tgw.id
+
+  # Disable association with default route table
+  transit_gateway_default_route_table_association = false
+
+  # Disable propagation to default route table
+  transit_gateway_default_route_table_propagation = false
 
   tags = {
     Name = format("attachment-%s", var.vpc_name)
