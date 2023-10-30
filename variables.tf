e4dd37a309ee5c89e0b061ca6daf51318416a4a1
Luis Mazariegos <luis.mazariegos@live.com>
2023-10-30 17:32:33 -0400

    changed region to us-east-1

--- variables.tf
+++ variables.tf
@@ -1,34 +1,34 @@
 variable "access_key" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "secret_key" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "region" {
-  type        = string
-  default     = "us-east-2"
+  type    = string
+  default = "us-east-1"
 }
 
 variable "vpc_name" {
-  description  = "Name of AWS VPC"
-  type         = string
+  description = "Name of AWS VPC"
+  type        = string
 }
 
 variable "vpc_cidr" {
-  description  = "CIDR of AWS VPC"
-  type         = string
+  description = "CIDR of AWS VPC"
+  type        = string
 }
 
 variable "subnet_name" {
-  description  = "Name of AWS subnet"
-  type         = string
+  description = "Name of AWS subnet"
+  type        = string
 }
 
 variable "subnet_cidr" {
-  description  = "CIDR of AWS subnet"
-  type         = string
+  description = "CIDR of AWS subnet"
+  type        = string
 }
