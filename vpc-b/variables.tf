50e00ed52cb965520b2df3f6b8e85a1739d9223b
Luis Mazariegos <luis.mazariegos@live.com>
2023-10-30 21:41:16 -0400

    Modified instance and region

--- vpc-b/variables.tf
+++ vpc-b/variables.tf
@@ -1,37 +1,37 @@
 # security credential variables
 variable "access_key" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "secret_key" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "region" {
-  type        = string
-  default     = "us-east-2"
+  type    = string
+  default = "us-east-1"
 }
 
 variable "vpc_cidr" {
-  description  = "CIDR of AWS VPC"
-  type         = string
+  description = "CIDR of AWS VPC"
+  type        = string
 }
 
 # instance variables
 variable "instance_type" {
-  description  = "Type of AWS instance to deploy"
-  type         = string
+  description = "Type of AWS instance to deploy"
+  type        = string
 }
 
 variable "ami_id" {
-  description  = "ID of amazon machine image"
-  type         = string
+  description = "ID of amazon machine image"
+  type        = string
 }
 
 variable "public_key" {
-  description  = "Path to public key"
-  type         = string
-  sensitive    = true
+  description = "Path to public key"
+  type        = string
+  sensitive   = true
 }
